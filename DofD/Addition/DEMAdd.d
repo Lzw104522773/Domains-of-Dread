5d615ff8ef1ec068b193235538dc64831359d1cf
11jo <79876551+11jo@users.noreply.github.com>
2024-01-21 13:49:23 +0100

    Fix
    - Scrolls wasn't really cast spell
    
    - Bams
    
    - Prevent issue for second componant if Player1 is not in Adventurer Mart when talking to Utterly Insane Person with misc42 in party inventory

--- DofD/Addition/DEMAdd.d
+++ DofD/Addition/DEMAdd.d
@@ -2,6 +2,9 @@
 // Add variable for Demdud.baf
 
 EXTEND_TOP DEMDUD 5
+  IF ~Global("Demodia","GLOBAL",0)~ THEN DO ~SetGlobal("Demodia","GLOBAL",1)~ EXIT
   IF ~See(Player1) PartyHasItem("MISC42") Global("Demodia","GLOBAL",1)~ THEN DO ~SetGlobal("Demodia","GLOBAL",2)~ EXIT
   IF ~!See(Player1) PartyHasItem("MISC42") Global("Demodia","GLOBAL",1)~ THEN EXIT
+  IF ~!PartyHasItem("MISC42") Global("Demodia","GLOBAL",1)~ THEN EXIT
+  IF ~GlobalGT("Demodia","GLOBAL",1)~ THEN EXIT
 END
